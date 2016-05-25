07cdefab4a65afc3ea8b33365e57b097bd0af587
vquilon <v.quilonr@gmail.com>
2016-05-25 21:33:03 +0200

    Tema 13: Mensajes Flash

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var partials = require('express-partials');
+var flash = require('express-flash');
 
 var routes = require('./routes/index');
 
@@ -14,7 +16,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(partials());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,7 +23,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));//cambiado a true en el tema 12
 app.use(cookieParser());
+app.use(session({secret: "Quiz 2016",
+                  resave:false,
+                  saveUninitialized: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(partials());
+app.use(flash());
 
 app.use('/', routes);
 
