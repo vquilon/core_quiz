30836484d57d2989ea9f2b3e856010b58d7bdf8b
vquilon <v.quilonr@gmail.com>
2016-05-26 15:24:39 +0200

    Tema 17: Migraciones y Seeders(paso 4.1)

---
+++ seeders/20160526142737-FillQuizzesTable.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Quizzes',[
+      { question: 'Capital de Italia', answer: 'Roma',
+        createdAt: new Date(), updateAt: new Date() },
+      { question: 'Capital de Portugal', answer: 'Lisboa',
+        createdAt: new Date(), updateAt: new Date() },
+      { question: 'Capital de España', answer: 'Madrid',
+        createdAt: new Date(), updateAt: new Date() }
+    ]);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Quizzes', null, {});
+  }
+};
